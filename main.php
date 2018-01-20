d4e63be06a086b37c5e51220d40cf40578339340
Adria <adriavelardos@gmail.com>
2018-01-21 00:12:26 +0100

    [GILDEDROSE] split into private methods to increase and decrease quality, to sellindown, and wth an instance as a parameter for updateQuality, this part is pairprogrammed

--- main.php
+++ main.php
@@ -14,7 +14,8 @@ $items []= new MPWAR5\GildedRoseKata\Item("Sulfuras, Hand of Ragnaros", 0, 80);
 $items []= new MPWAR5\GildedRoseKata\Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
 $items []= new MPWAR5\GildedRoseKata\Item("Conjured Mana Cake", 3, 6);
 
-MPWAR5\GildedRoseKata\GildedRose::updateQuality($items);
+$gildedRose = new MPWAR5\GildedRoseKata\GildedRose();
+MPWAR5\GildedRoseKata\GildedRose::updateQuality($gildedRose, $items);
 
 foreach ($items as $item) {
 	echo "Item: {$item->name}, Quality: {$item->quality}, SellIn: {$item->sellIn}\n";
--- src/GildedRose.php
+++ src/GildedRose.php
@@ -3,62 +3,71 @@
 namespace MPWAR5\GildedRoseKata;
 
 class GildedRose {
+	
+	   private $item;
+	   private $itemName;
+	   private $quality;
+	   private $sellIn;
+	   private $maxQuality = 50;
+	
+	   private function qualityUp($increment)
+	   {
+		   if ($this->quality < $this->maxQuality) {
+			   $this->item->setQuality( $this->quality + $increment);
+		   }
+	   }
+	
+	   private function qualityDown($decrement)
+	   {
+		   if ($this->quality <= 0) return;
+		   $this->item->setQuality( $this->quality - $decrement );
+	   }
+	
+	   private function sellInDown()
+	   {
+		   if ($this->sellIn <= 0) return;
+		   $this->item->setQuality( $this->quality - 1 );
+	   }
+	
+	   public static function updateQuality(
+		   GildedRose $gildedRoseInstance,
+		   $items
+	   ) {
+		   for ($i = 0; $i < count($items); $i++) {
+			   $agedBrieName = "Aged Brie";
+			   $backstagePassesName = "Backstage passes to a TAFKAL80ETC concert";
+			   $sulfurasName = "Sulfuras, Hand of Ragnaros";
 
-	public static function updateQuality(
-		$items
-	) {
-		for ($i = 0; $i < count($items); $i++) {
-			$itemName = $items[$i]->getName();
-			$agedBrieName = "Aged Brie";
-			$backstagePassesName = "Backstage passes to a TAFKAL80ETC concert";
-			$sulfurasName = "Sulfuras, Hand of Ragnaros";
-			if (($agedBrieName != $itemName) && ($backstagePassesName != $itemName)) {
-				if ($items[$i]->getQuality() > 0) {
-					if ($sulfurasName != $itemName) {
-						$items[$i]->setQuality($items[$i]->getQuality() - 1);
-					}
-				}
-			} else {
-				if ($items[$i]->getQuality() < 50) {
-					$items[$i]->setQuality($items[$i]->getQuality() + 1);
-					if ($backstagePassesName == $itemName) {
-						if ($items[$i]->getSellIn() < 11) {
-							if ($items[$i]->getQuality() < 50) {
-								$items[$i]->setQuality($items[$i]->getQuality() + 1);
-							}
-						}
-						if ($items[$i]->getSellIn() < 6) {
-							if ($items[$i]->getQuality() < 50) {
-								$items[$i]->setQuality($items[$i]->getQuality() + 1);
-							}
-						}
-					}
-				}
-			}
-
-			if ($sulfurasName != $itemName) {
-				$items[$i]->setSellIn($items[$i]->getSellIn() - 1);
-			}
-
-			if ($items[$i]->getSellIn() < 0) {
-				if ($agedBrieName != $itemName) {
-					if ($backstagePassesName != $itemName) {
-						if ($items[$i]->getQuality() > 0) {
-							if ($sulfurasName != $itemName) {
-								$items[$i]->setQuality($items[$i]->getQuality() - 1);
-							}
-						}
-					} else {
-						$items[$i]->setQuality($items[$i]->getQuality() - $items[$i]->getQuality());
-					}
-				} else {
-					if ($items[$i]->getQuality() < 50) {
-						$items[$i]->setQuality($items[$i]->getQuality() + 1);
-					}
-				}
-			}
-		}
-	}
-}
+			   $gildedRoseInstance->item = $items[$i];
+			   $gildedRoseInstance->itemName = $items[$i]->getName();
+			   $gildedRoseInstance->quality = $items[$i]->getQuality();
+			   $gildedRoseInstance->sellIn = $items[$i]->getSellIn();
+	
+			   if ($gildedRoseInstance->itemName == $agedBrieName) {
+				   $gildedRoseInstance->qualityUp(1);
+			   }
+	
+			   else if ($gildedRoseInstance->itemName == $sulfurasName) {
+				   continue;
+			   }
+	
+			   else if ($gildedRoseInstance->itemName == $backstagePassesName) {
+				   if($gildedRoseInstance->sellIn == 0) {
+					   $this->item.setQuality(0);
+				   }
+				   else if($gildedRoseInstance->sellIn <= 5) {
+					   $this->qualityUp(3);
+				   }
+				   else if($gildedRoseInstance->sellIn <= 10) {
+					   $this->qualityUp(2);
+				   }
+			   }
+			   else {
+				   $gildedRoseInstance->qualityDown(1);
+			   }
+			   $gildedRoseInstance->sellInDown();
+		   }
+	   }
+   }
 
 ?>
