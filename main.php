6e596c0ed9509ba66710d2c7849e53540e7d82f1
Adria <adriavelardos@gmail.com>
2018-01-22 21:12:07 +0100

    Corrections from class, still not the expected result

--- main.php
+++ main.php
@@ -5,20 +5,20 @@ ini_set("display_errors", 1);
 
 require_once 'vendor/autoload.php';
 
+use MPWAR5\GildedRoseKata\Item;
+use MPWAR5\GildedRoseKata\GildedRose;
 $items = array();
 
-$items []= new MPWAR5\GildedRoseKata\Item("+5 Dexterity Vest", 10, 20);
-$items []= new MPWAR5\GildedRoseKata\Item("Aged Brie", 2, 0);
-$items []= new MPWAR5\GildedRoseKata\Item("Elixir of the Mongoose", 5, 7);
-$items []= new MPWAR5\GildedRoseKata\Item("Sulfuras, Hand of Ragnaros", 0, 80);
-$items []= new MPWAR5\GildedRoseKata\Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
-$items []= new MPWAR5\GildedRoseKata\Item("Conjured Mana Cake", 3, 6);
+$items []= new Item("+5 Dexterity Vest", 10, 20);
+$items []= new Item("Aged Brie", 2, 0);
+$items []= new Item("Elixir of the Mongoose", 5, 7);
+$items []= new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+$items []= new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+$items []= new Item("Conjured Mana Cake", 3, 6);
 
-$gildedRose = new MPWAR5\GildedRoseKata\GildedRose();
-MPWAR5\GildedRoseKata\GildedRose::updateQuality($gildedRose, $items);
+$gildedRose = new GildedRose();
+$gildedRose->updateQuality(...$items);
 
 foreach ($items as $item) {
 	echo "Item: {$item->name}, Quality: {$item->quality}, SellIn: {$item->sellIn}\n";
 }
-
-?>
--- src/GildedRose.php
+++ src/GildedRose.php
@@ -1,73 +1,68 @@
 <?php
-
+declare(strict_types=1);
 namespace MPWAR5\GildedRoseKata;
 
-class GildedRose {
-	
-	   private $item;
-	   private $itemName;
-	   private $quality;
-	   private $sellIn;
-	   private $maxQuality = 50;
-	
-	   private function qualityUp($increment)
-	   {
-		   if ($this->quality < $this->maxQuality) {
-			   $this->item->setQuality( $this->quality + $increment);
-		   }
-	   }
-	
-	   private function qualityDown($decrement)
-	   {
-		   if ($this->quality <= 0) return;
-		   $this->item->setQuality( $this->quality - $decrement );
-	   }
-	
-	   private function sellInDown()
-	   {
-		   if ($this->sellIn <= 0) return;
-		   $this->item->setQuality( $this->quality - 1 );
-	   }
-	
-	   public static function updateQuality(
-		   GildedRose $gildedRoseInstance,
-		   $items
-	   ) {
-		   for ($i = 0; $i < count($items); $i++) {
-			   $agedBrieName = "Aged Brie";
-			   $backstagePassesName = "Backstage passes to a TAFKAL80ETC concert";
-			   $sulfurasName = "Sulfuras, Hand of Ragnaros";
+class GildedRose
+{
+    private $item;
+    private $itemName;
+    private $quality;
+    private $sellIn;
+    private $maxQuality = 50;
 
-			   $gildedRoseInstance->item = $items[$i];
-			   $gildedRoseInstance->itemName = $items[$i]->getName();
-			   $gildedRoseInstance->quality = $items[$i]->getQuality();
-			   $gildedRoseInstance->sellIn = $items[$i]->getSellIn();
-	
-			   if ($gildedRoseInstance->itemName == $agedBrieName) {
-				   $gildedRoseInstance->qualityUp(1);
-			   }
-	
-			   else if ($gildedRoseInstance->itemName == $sulfurasName) {
-				   continue;
-			   }
-	
-			   else if ($gildedRoseInstance->itemName == $backstagePassesName) {
-				   if($gildedRoseInstance->sellIn == 0) {
-					   $gildedRoseInstance->item.setQuality(0);
-				   }
-				   else if($gildedRoseInstance->sellIn <= 5) {
-					   $gildedRoseInstance->qualityUp(3);
-				   }
-				   else if($gildedRoseInstance->sellIn <= 10) {
-					   $gildedRoseInstance->qualityUp(2);
-				   }
-			   }
-			   else {
-				   $gildedRoseInstance->qualityDown(1);
-			   }
-			   $gildedRoseInstance->sellInDown();
-		   }
-	   }
-   }
+    private function qualityUp($increment)
+    {
+        if ($this->quality < $this->maxQuality) {
+            $this->item->setQuality($this->quality + $increment);
+        }
+    }
 
-?>
+    private function qualityDown($decrement)
+    {
+        if ($this->quality <= 0) return;
+        $this->item->setQuality($this->quality - $decrement);
+    }
+
+    private function sellInDown()
+    {
+        if ($this->sellIn <= 0) return;
+        $this->item->setQuality($this->quality - 1);
+    }
+
+    public function updateQuality(
+        Item ...$items
+    ): void{
+        foreach($items as $item){
+            $this->updateItemQuality($item);
+        }
+    }
+
+    public function updateItemQuality(Item $item): void
+    {
+        $agedBrieName = "Aged Brie";
+        $backstagePassesName = "Backstage passes to a TAFKAL80ETC concert";
+        $sulfurasName = "Sulfuras, Hand of Ragnaros";
+
+        $this->item = $item;
+        $this->itemName = $item->getName();
+        $this->quality = $item->getQuality();
+        $this->sellIn = $item->getSellIn();
+
+        if ($this->itemName == $agedBrieName) {
+            $this->qualityUp(1);
+        } else if ($this->itemName == $sulfurasName) {
+            $this->item->setQuality($this->item->getQuality());
+        } else if ($this->itemName == $backstagePassesName) {
+            if ($this->sellIn == 0) {
+                $this->item->setQuality(0);
+            } else if ($this->sellIn <= 5) {
+                $this->qualityUp(3);
+            } else if ($this->sellIn <= 10) {
+                $this->qualityUp(2);
+            }
+        } else {
+            $this->qualityDown(1);
+        }
+        $this->sellInDown();
+    }
+}
